d600c5584340ffce05b616fccc4c1deb6bf833a9
tillmanjr <tillman_jr@yahoo.com>
2018-10-14 06:02:43 -0700

    initial working Binary Search Tree

---
+++ src/tdjr-bst-kvp.js
@@ -0,0 +1,51 @@
+'use strict;'
+
+const {
+    createTdjrBST,
+    ComparisonResult
+} = require('./tdjr-bst')
+
+/**
+ * An alternate node.data which allows storing data (value) separate from the 
+ * value (key) used to sort it.
+ * Note: ensure the BinarySearchTree is created with a compareNodeData function
+ *       which uses the key for comparison purposes, e.g. compareNodeDataKVP
+ * @param {*} key 
+ * @param {*} value 
+ */
+const createNodeDataKVP = (
+    key,
+    value
+) => {
+    return {
+        'key': key,
+        'value': value,
+        'toString': () => `Key: ${key}, Value: ${value}`
+    }
+}
+
+/**
+ * Comparison function for data objects having a key property
+ *   Comparison is performed using the key property of each object
+ * @param {*} lhs 
+ * @param {*} rhs 
+ */
+const compareNodeDataKVP = (lhs, rhs) => {
+    if (lhs.key < rhs.key) {
+        return ComparisonResult.LESS_THAN
+    }
+    if (lhs.key > rhs.key) {
+        return ComparisonResult.GREATER_THAN
+    }
+    return ComparisonResult.EQUAL_TO
+}
+
+const createTdjrBSTKVP = (compareNodeDataFn = compareNodeDataKVP) => {
+    return createTdjrBST(compareNodeDataFn)
+}
+
+module.exports = {
+    createNodeDataKVP,
+    compareNodeDataKVP,
+    createTdjrBSTKVP
+}
